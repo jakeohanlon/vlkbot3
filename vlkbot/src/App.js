f1e238f61fdb8706db3e4abe6d91c8182f5cbe36
Jake <88779170+jakeohanlon@users.noreply.github.com>
2023-07-23 01:28:27 +0800

    vlkbot3
    works

---
+++ vlkbot/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const userMessage = { text: inputValue, sender: 'user' };
+      const botMessage = { text: "Ian, please don't talk to me", sender: 'bot' };
+      const newMessages = [...messages, userMessage, botMessage];
+      setMessages(newMessages);
+      setInputValue('');
+      console.log(newMessages); // Check the messages array
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Vlkbot</h1>
+      <div className="messages-container">
+        {messages.map((message, index) => {
+          console.log(message.text); // Check the text property
+          return (
+            <div
+              key={index}
+              className={`message ${message.sender === 'bot' ? 'bot' : 'user'}`}
+            >
+              {message.text}
+            </div>
+          );
+        })}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message here"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
